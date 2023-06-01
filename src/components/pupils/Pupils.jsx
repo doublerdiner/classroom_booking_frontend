57154d740364ba79d9e744e32103fb5cc5327327
Chris Barclay <chris.barclay1@gmail.com>
2023-06-01 17:02:57 +0100

    grid added to pupils file

--- src/components/pupils/Pupils.jsx
+++ src/components/pupils/Pupils.jsx
@@ -1,10 +1,20 @@
 import { Typography } from "@mui/material"
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
+import AddPupil from "./AddPupil"
+import PupilSearch from "./PupilSearch"
 
 const Pupils = ({allStudents})=>{
     
     return (
         <>
-            <Typography>Hi</Typography>
+        <Grid2 container spacing={5}>
+            <Grid2 xs={3}>
+                <AddPupil/>
+            </Grid2>
+            <Grid2 xs={3}>
+                <PupilSearch/>
+            </Grid2>
+        </Grid2>
         </>
     )
 }
